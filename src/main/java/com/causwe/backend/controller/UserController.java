08aa8a0cfa81d4ad999b47f792e1a88d3e7e51f9
kendsi <tjrwls1832@gmail.com>
2024-05-24 03:18:01 +0900

    commit

--- src/main/java/com/causwe/backend/controller/UserController.java
+++ src/main/java/com/causwe/backend/controller/UserController.java
@@ -1,14 +1,10 @@
 package com.causwe.backend.controller;
 
 import com.causwe.backend.exceptions.UnauthorizedException;
-
-import com.causwe.backend.dto.UserDTO;
 import com.causwe.backend.model.User;
 import com.causwe.backend.service.UserService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
-
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/users")
@@ -25,33 +20,31 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    private ModelMapper modelMapper;
-
     @PostMapping("/signup")
-    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO, @CookieValue(value = "memberId", required = false) Long memberId) {
+    public ResponseEntity<User> createUser(@RequestBody User user, @CookieValue(value = "memberId", required = false) Long memberId) {
         User currentUser = userService.getUserById(memberId);
         if (currentUser == null || currentUser.getRole() != User.Role.ADMIN) {
             throw new UnauthorizedException("Only admins can create users.");
         }
 
-        User newUser = userService.createUser(modelMapper.map(userDTO, User.class));
+        User newUser = userService.createUser(user);
         if (newUser == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        UserDTO newUserDTO = modelMapper.map(newUser, UserDTO.class);
-
-        return new ResponseEntity<>(newUserDTO, HttpStatus.CREATED);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
-    //TODO login responseBody는 Map<memberID.toString(), User>의 형태인데 DTO로 변환이 필요한지 결정
     @PostMapping("/login")
-    public ResponseEntity<Map<String, Object>> login(@RequestBody UserDTO userDTO, HttpServletResponse response) {
-        Map<String, Object> responseBody = userService.login(modelMapper.map(userDTO, User.class));
+    public ResponseEntity<Map<String, Object>> login(@RequestBody User user, HttpServletResponse response) {
+        Map<String, Object> responseBody = userService.login(user);
         if (responseBody != null) {
             Cookie cookie = new Cookie("memberId", responseBody.get("memberId").toString());
             cookie.setPath("/");
-            response.addCookie(cookie);
+            cookie.setHttpOnly(true);
+            cookie.setSecure(true);
+            String cookieHeader = String.format("%s=%s; Path=%s; SameSite=None; Secure", cookie.getName(), cookie.getValue(), cookie.getPath());
+            response.addHeader("Set-Cookie", cookieHeader);
             return new ResponseEntity<>(responseBody, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
@@ -59,15 +52,8 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<List<UserDTO>> getAllDevs() {
+    public ResponseEntity<List<User>> getAllDevs() {
         List<User> devUsers = userService.getAllDevs();
-
-        List<UserDTO> devUserDTOs = devUsers
-        .stream()
-        .map(user -> modelMapper.map(user, UserDTO.class))
-        .collect(Collectors.toList());
-
-
-        return new ResponseEntity<>(devUserDTOs, HttpStatus.OK);
+        return new ResponseEntity<>(devUsers, HttpStatus.OK);
     }
 }
